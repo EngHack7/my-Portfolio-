952226adcf6e490a64fe789cc58e5bd7c53be35f
EngHack7 <hamzaz.k.hasan@st.ahu.edu.jo>
2020-06-12 07:34:33 +0300

    add name and git Link

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,7 @@ function Header () {
            <ul className="ul_nav">
             <li className="li_nav"><a href="https://web.facebook.com/profile.php?id=100002411706224">FaceBook</a></li>
             <li className="li_nav"><a href="https://www.linkedin.com/in/hamza-kamel-3963b61b0/">Linked in</a></li>
+            <li className="li_nav"><a href="https://github.com/EngHack7">github</a></li>
            </ul> 
         </div>
     );
